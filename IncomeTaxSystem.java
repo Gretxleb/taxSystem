d9e9da352fcb1ec88bff0ed191c919e905def56d
PAUL.s <96791484+Gretxleb@users.noreply.github.com>
2025-08-25 18:24:31 +0500

    Update IncomeTaxSystem.java

--- IncomeTaxSystem.java
+++ IncomeTaxSystem.java
@@ -3,7 +3,6 @@ package taxes;
 public class IncomeTaxSystem extends TaxSystem {
     @Override
     public int calcTaxFor(int debit, int credit) {
-        // Налог 6% от доходов
         return (int) (debit * 0.06);
     }
 }
