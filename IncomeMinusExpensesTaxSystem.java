d68ab2216962e450089b0181dbd212bf6970fb1e
PAUL.s <96791484+Gretxleb@users.noreply.github.com>
2025-08-25 18:24:09 +0500

    Update IncomeMinusExpensesTaxSystem.java

--- IncomeMinusExpensesTaxSystem.java
+++ IncomeMinusExpensesTaxSystem.java
@@ -3,9 +3,7 @@ package taxes;
 public class IncomeMinusExpensesTaxSystem extends TaxSystem {
     @Override
     public int calcTaxFor(int debit, int credit) {
-        // Рассчитываем разницу
         int profit = debit - credit;
-        // Налог 15% от разницы, если она положительная
         if (profit > 0) {
             return (int) (profit * 0.15);
         } else {
